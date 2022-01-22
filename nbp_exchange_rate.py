13efd7183b320cca732f9a74f7a989a4d3b69ba7
Maciej Ramotowski <ramotowski.maciej@gmail.com>
2022-01-22 17:52:08 +0100

    Change how currency is checked.
    Create tuple with all currencies from table A.
    Use it to check if provided currency is proper one.

--- nbp_exchange_rate.py
+++ nbp_exchange_rate.py
@@ -5,6 +5,18 @@ from flask_restful import reqparse
 from datetime import date, datetime, timedelta
 import json
 
+def get_currencies_codes():
+    table_a_addr = f'https://api.nbp.pl/api/exchangerates/tables/a'
+    request = requests.get(table_a_addr)
+    table_a = json.loads(request.text)
+    currencies = table_a[0]['rates']
+    currencies_codes = set()
+
+    for currency in currencies:
+        currencies_codes.add(currency['code'])
+
+    return tuple(currencies_codes)
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -13,12 +25,13 @@ parser.add_argument('currency', type=str, help='Foreign currency of obtained inc
 parser.add_argument('date', type=str, help='Day of obtaining the income')
 
 class ExchangeRate(Resource):
+    def __init__(self, currencies_codes):
+        self.currencies_codes = currencies_codes
+
     def get(self):
         args = parser.parse_args()
-        nbpApiAddress = f'https://api.nbp.pl/api/exchangerates/rates/a/{args.currency}'
 
-        request = requests.get(nbpApiAddress)
-        if request.status_code > 299:
+        if args.currency.upper() not in self.currencies_codes:
             return {'message': '400 Bad Request', 'error': 'Incorrect currency.'}, 400
 
         try:
@@ -30,6 +43,7 @@ class ExchangeRate(Resource):
         if dateValue > datetime.now().date() or date.fromisoformat('2002-01-02') > dateValue:
             return {'message': '400 bad request', 'error': 'Incorrect date. Correct date is between 2002-01-03 and present.'}, 400
 
+        nbpApiAddress = f'https://api.nbp.pl/api/exchangerates/rates/a/{args.currency}'
         for dayBefore in range(7):
             nbpApiAddress += f'/{dateValue}'
             request = requests.get(nbpApiAddress)
@@ -44,7 +58,9 @@ class ExchangeRate(Resource):
         message = json.loads(request.text)
         return {'message': f'{message}'}, 200
 
-api.add_resource(ExchangeRate, '/')
 
 if __name__ == '__main__':
+    currencies_codes = get_currencies_codes()
+    api.add_resource(ExchangeRate, '/',
+                     resource_class_kwargs={'currencies_codes': currencies_codes})
     app.run(host='0.0.0.0')
