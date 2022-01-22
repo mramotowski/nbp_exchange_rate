c670e62ca79c85da7e28db83b3ed36a929173e45
Maciej Ramotowski <ramotowski.maciej@gmail.com>
2022-01-22 18:19:24 +0100

    Use endpoint instead of reqparse

--- nbp_exchange_rate.py
+++ nbp_exchange_rate.py
@@ -17,40 +17,30 @@ def get_currencies_codes():
 
     return tuple(currencies_codes)
 
-app = Flask(__name__)
-api = Api(app)
-
-parser = reqparse.RequestParser()
-parser.add_argument('currency', type=str, help='Foreign currency of obtained income')
-parser.add_argument('date', type=str, help='Day of obtaining the income')
-
 class ExchangeRate(Resource):
     def __init__(self, currencies_codes):
         self.currencies_codes = currencies_codes
 
-    def get(self):
-        args = parser.parse_args()
-
-        if args.currency.upper() not in self.currencies_codes:
+    def get(self, currency, date_string):
+        if currency.upper() not in self.currencies_codes:
             return {'message': '400 Bad Request', 'error': 'Incorrect currency.'}, 400
 
         try:
-            dateValue = date.fromisoformat(args.date)
+            dateValue = date.fromisoformat(date_string)
         except ValueError:
             return {'message': '400 bad request', 'error': 'Incorrect date string format. It should be YYYY-MM-DD.'}, 400
 
-        dateValue -= timedelta(days=1)
         if dateValue > datetime.now().date() or date.fromisoformat('2002-01-02') > dateValue:
             return {'message': '400 bad request', 'error': 'Incorrect date. Correct date is between 2002-01-03 and present.'}, 400
 
-        nbpApiAddress = f'https://api.nbp.pl/api/exchangerates/rates/a/{args.currency}'
+        nbpApiAddress = f'https://api.nbp.pl/api/exchangerates/rates/a/{currency}'
         for dayBefore in range(7):
+            dateValue -= timedelta(days=1)
             nbpApiAddress += f'/{dateValue}'
             request = requests.get(nbpApiAddress)
             if request.status_code == 200:
                 break
             nbpApiAddress = nbpApiAddress.rsplit('/', 1)[0]
-            dateValue -= timedelta(days=1)
 
         if request.status_code != 200:
             return {'message': '404 Not Found', 'error': 'The server can not find the requested resource.'}, 404
@@ -61,6 +51,8 @@ class ExchangeRate(Resource):
 
 if __name__ == '__main__':
     currencies_codes = get_currencies_codes()
-    api.add_resource(ExchangeRate, '/',
+    app = Flask(__name__)
+    api = Api(app)
+    api.add_resource(ExchangeRate, '/prevday/exchangerate/<string:currency>/<string:date_string>',
                      resource_class_kwargs={'currencies_codes': currencies_codes})
     app.run(host='0.0.0.0')
