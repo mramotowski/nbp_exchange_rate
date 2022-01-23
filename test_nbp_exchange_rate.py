8d248454b4a259722bdd7952080b90f0a6926afc
Maciej Ramotowski <ramotowski.maciej@gmail.com>
2022-01-23 16:53:14 +0100

    Fix not working unittests
    Move flask app creation to function.

--- test_nbp_exchange_rate.py
+++ test_nbp_exchange_rate.py
@@ -1,70 +1,61 @@
 import unittest
-import nbp_exchange_rate
+from nbp_exchange_rate import create_app
 
-def template_test(app, data, status_code, expected_response):
-    response = app.client.get('/', query_string=data)
-    app.assertEqual(response.status_code, status_code)
-    app.assertDictEqual(response.get_json(), expected_response)
+def template_test(self, currency, date_string, status_code, expected_response):
+    response = self.client.get(f'/prevday/exchangerate/{currency}/{date_string}')
+    self.assertEqual(response.status_code, status_code)
+    self.assertDictEqual(response.get_json(), expected_response)
 
 class TestExchangeRate(unittest.TestCase):
     def setUp(self):
-        self.app = nbp_exchange_rate.app
-        self.client = self.app.test_client()
+        app = create_app()
+        self.client = app.test_client()
 
     def test_wrong_currency(self):
-        data = {
-            'currency': 'UDS',
-            'date': '2022-01-11'}
+        currency = 'UDS'
+        date_string = '2022-01-11'
         status_code = 400
         expected_response = {
             'message': '400 Bad Request',
             'error': 'Incorrect currency.'}
-        template_test(self, data, status_code, expected_response)
+        template_test(self, currency, date_string, status_code, expected_response)
 
     def test_date_format(self):
-        data = {
-            'currency': 'USD',
-            'date': ''}
+        currency = 'USD'
         expected_response = {
-            'message': '400 bad request',
+            'message': '400 Bad Request',
             'error': 'Incorrect date string format. It should be YYYY-MM-DD.'}
         status_code = 400
-        dates_to_test = ['2010/04/13', '2010.05.26']
-        for date in dates_to_test:
-            with self.subTest(date=date):
-                data['date'] = date
-                template_test(self, data, status_code, expected_response)
+        dates_to_test = ['2022 01 01', '2010.05.26']
+        for date_string in dates_to_test:
+            with self.subTest(date=date_string):
+                template_test(self, currency, date_string, status_code, expected_response)
 
     def test_date_not_in_range(self):
-        data = {
-            'currency': 'USD',
-            'date': ''}
+        currency = 'USD'
         expected_response = {
-            'message': '400 bad request',
+            'message': '400 Bad Request',
             'error': 'Incorrect date. Correct date is between 2002-01-03 and present.'}
         status_code = 400
 
         dates_to_test = ['3210-01-13', '1926-05-26']
-        for date in dates_to_test:
-            with self.subTest(date=date):
-                data['date'] = date
-                template_test(self, data, status_code, expected_response)
+        for date_string in dates_to_test:
+            with self.subTest(date=date_string):
+                template_test(self, currency, date_string, status_code, expected_response)
 
     def test_day_after_wroking_day(self):
-        data = {
-            'currency': 'USD',
-            'date': '2022-01-13'}
+        currency = 'USD'
+        date_string = '2022-01-13'
         expected_response = {'message': '{\'table\': \'A\', \'currency\': \'dolar ameryka\u0144ski\', \'code\': \'USD\', \'rates\': [{\'no\': \'007/A/NBP/2022\', \'effectiveDate\': \'2022-01-12\', \'mid\': 3.9879}]}'}
         status_code = 200
-        template_test(self, data, status_code, expected_response)
+        template_test(self, currency, date_string, status_code, expected_response)
 
     def test_day_after_free_day(self):
-        data = {
-            'currency': 'USD',
-            'date': '2022-01-11'}
+        currency = 'USD'
+        date_string = '2022-01-11'
         expected_response = {"message": "{'table': 'A', 'currency': 'dolar ameryka\u0144ski', 'code': 'USD', 'rates': [{'no': '005/A/NBP/2022', 'effectiveDate': '2022-01-10', 'mid': 4.0064}]}"}
         status_code = 200
-        template_test(self, data, status_code, expected_response)
+        template_test(self, currency, date_string, status_code, expected_response)
 
 if __name__ == "__main__":
     unittest.main()
