6c0284a6e74433a9a839d7d8e7d9669db89646ce
mramotowski <ramotowski.maciej@gmail.com>
2022-02-04 21:17:39 +0100

    Improvements (#13)
    * app: Logging - change logs format to json
    
    * tests: Disable logging
    
    * app: Fix error handling
    
    * app: Add 200 response to logs
    
    * utils: Add missing requirement
    
    * app: Move ExchangeRate class to resources dir
    
    * docs: Rename repository

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM ubuntu:latest
 
-COPY requirements.txt requirements.txt
-COPY nbp_exchange_rate.py nbp_exchange_rate.py
-
 RUN apt-get update && apt-get install -y \
   python3-pip \
   && rm -rf /var/lib/apt/lists/*
 
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
-ENTRYPOINT ["python3", "nbp_exchange_rate.py"]
+COPY pln_exchange_rate.py pln_exchange_rate.py
+COPY resources/ resources/
+
+ENTRYPOINT ["python3", "pln_exchange_rate.py"]
---
+++ pln_exchange_rate.py
@@ -0,0 +1,66 @@
+import logging
+import json
+from logging.handlers import RotatingFileHandler
+import sys
+import requests
+from flask import Flask
+from flask_restful import Api
+from jsonformatter import basicConfig
+from paste.translogger import TransLogger
+from waitress import serve
+from resources.exchangerate import ExchangeRate
+
+def get_currencies_codes():
+    table_a_addr = f'https://api.nbp.pl/api/exchangerates/tables/a'
+    try:
+        req = requests.get(table_a_addr)
+    except requests.exceptions.RequestException:
+        return None
+
+    if req.status_code != 200:
+        return None
+
+    try:
+        table_a = json.loads(req.text)
+    except json.decoder.JSONDecodeError:
+        logging.error('Couldn\'t load json - table A.')
+        return None
+
+    currencies = table_a[0]['rates']
+    currencies_codes = set()
+
+    for currency in currencies:
+        currencies_codes.add(currency['code'])
+
+    return tuple(currencies_codes)
+
+def create_app():
+    currencies_codes = get_currencies_codes()
+    if not currencies_codes:
+        logging.error('Couldn\'t create currencies code list for table A.')
+        return None
+    app = Flask(__name__)
+    api = Api(app)
+    api.add_resource(ExchangeRate, '/prevday/exchangerate/<string:currency>/<string:date_string>',
+                     resource_class_kwargs={'currencies_codes': currencies_codes})
+    return app
+
+if __name__ == '__main__':
+    STRING_FORMAT = '''{
+        "asctime":         "asctime",
+        "levelname":       "levelname",
+        "name":            "name",
+        "message":         "message"
+    }'''
+    file_handler = RotatingFileHandler('pln_exchange_rate.log', maxBytes=100*10**6, backupCount=1)
+    basicConfig(format=STRING_FORMAT, handlers=[file_handler, logging.StreamHandler()])
+    logging.getLogger().setLevel(logging.INFO)
+
+    app = create_app()
+    if not app:
+        sys.exit(1)
+
+    app_logged = TransLogger(app, setup_console_handler=False)
+    serve(app_logged, host='0.0.0.0', port='5000')
+
+    sys.exit(0)
